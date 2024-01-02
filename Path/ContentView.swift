f0be8420e815b0d0a51d1d7860bfd6e4533e67e2
Tim Halli <t.timhalli@gmail.com>
2024-01-02 17:40:07 +0200

    Refactor the code

--- Path/ContentView.swift
+++ Path/ContentView.swift
@@ -14,30 +14,34 @@ struct ContentView: View {
             let height = geometry.size.height * 0.1
             let rectangle = CGRect(x: .zero, y: .zero, width: width , height: height)
             
-            VStack(spacing: 0) {
-                ForEach(1..<10, id: \.self) { columnIndex in
+            ScrollView {
+                Spacer()
+                LazyVGrid(columns: [GridItem(.fixed(20))], spacing: 0, content: {
                     
-                    HStack(spacing: 0) {
-                        ForEach(1..<10, id: \.self) { rowIndex in
-                            
-                            Path { path in
-                                path.addRect(rectangle)
-                            }
-                            .fill((columnIndex + rowIndex) % 2 == 0 ? .yellow : .white)
-                            .border(.gray, width: 1)
-                            .frame(width: width, height: height)
-                            .overlay(
-                                Text("\(columnIndex * rowIndex)")
-                                    .foregroundColor(.black)
-                                    .bold()
-                            )
+                    ForEach(1..<10, id: \.self) { columnIndex in
+                        
+                        HStack(spacing: 0) {
+                            LazyHGrid(rows: [GridItem(.fixed(20))], spacing: 0, content: {
+                                ForEach(1..<10, id: \.self) { rowIndex in
+                                    
+                                    Path { path in
+                                        path.addRect(rectangle)
+                                    }
+                                    .fill((columnIndex + rowIndex) % 2 == 0 ? .yellow : .white)
+                                    .border(.gray, width: 1)
+                                    .frame(width: width, height: height)
+                                    .overlay(
+                                        Text("\(columnIndex * rowIndex)")
+                                            .foregroundColor(.black)
+                                            .bold()
+                                    )
+                                }
+                            })
                         }
                     }
-                }
-               
+                })
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            
         }
     }
 }
